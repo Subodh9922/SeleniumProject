a541f76d6b8297e11a92fd221d5a0e132f9df5d4
Subodh9922 <subodhbudhe@gmail.com>
2022-10-20 18:35:17 +0530

    changes done

--- src/main/java/com/fb/AccessModifiers/ArithmeticOperation.java
+++ src/main/java/com/fb/AccessModifiers/ArithmeticOperation.java
@@ -1,2 +1,32 @@
-package com.fb.AccessModifiers;public class ArithmeticOperation {
+package com.fb.AccessModifiers;
+
+public class ArithmeticOperation {
+    int a;
+    int b;
+    int c;
+    int result;
+    int result1;
+    void addition(int a, int b){
+        result = a+b;
+    }
+
+    void addition( int a, int b, int c){
+        result1 = a+b+c;
+    }
+
+    void printResult(){
+        System.out.println("the result is : " +result);
+    }
+
+    void printResult1(){
+        System.out.println("the result is : " +result1);
+    }
+
+    public static void main(String[] args) {
+        ArithmeticOperation obj= new ArithmeticOperation();
+        obj.addition(20,40);
+        obj.printResult();
+        obj.addition(30,60,80);
+        obj.printResult1();
+    }
 }
