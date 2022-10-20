e87738c8ead0e2d7278369f307c9ad38f763e15a
Subodh9922 <subodhbudhe@gmail.com>
2022-10-20 18:45:33 +0530

    cha

--- src/main/java/com/fb/AccessModifiers/ArithmeticOperation.java
+++ src/main/java/com/fb/AccessModifiers/ArithmeticOperation.java
@@ -6,12 +6,18 @@ public class ArithmeticOperation {
     int c;
     int result;
     int result1;
+
+    double result2;
     void addition(int a, int b){
         result = a+b;
     }
 
     void addition( int a, int b, int c){
         result1 = a+b+c;
+    }
+
+    void addition(int a, int b, int c, double a1){
+        result2= a+b+c+a1;
     }
 
     void printResult(){
@@ -22,11 +28,17 @@ public class ArithmeticOperation {
         System.out.println("the result is : " +result1);
     }
 
+    void printResult2(){
+        System.out.println("the result is : "+ result2);
+    }
+
     public static void main(String[] args) {
         ArithmeticOperation obj= new ArithmeticOperation();
         obj.addition(20,40);
         obj.printResult();
         obj.addition(30,60,80);
         obj.printResult1();
+        obj.addition(49,67,70,98.77);
+        obj.printResult2();
     }
 }
